50a847746a324881edf0ed230c848ba904ae9094
Giovanni <g.mancini.0@gmail.com>
2021-11-11 15:42:42 +0100

    [CEN-961] Separate command to read and update award winners

--- src/cstar-cli/cstar/cli/bpd/awardwinner.py
+++ src/cstar-cli/cstar/cli/bpd/awardwinner.py
@@ -16,15 +16,12 @@ select
   bc.payoff_instr_s as payoff_instr,
 	1500 as amount_n,
 	CURRENT_TIMESTAMP as insert_date_t,
-	'update_bpd_award_winner' as insert_user_s,
+	'update_bpd_award_winner_supercashback' as insert_user_s,
 	true as enabled_b,
 	bap.aw_period_start_d,
 	bap.aw_period_end_d,
 	1500 as jackpot_n,
-	case
-		when bcr.cashback_n <= bre.period_cashback_max_n then bcr.cashback_n
-		else bre.period_cashback_max_n
-	end as cashback_n,
+	0 as cashback_n,
 	'02' as typology_s,
 	bc.account_holder_cf_s,
 	bc.account_holder_name_s,
@@ -44,8 +41,33 @@ where
 	bcr.award_period_id_n = %(award_period)s
 	and bcr.transaction_n >= bap.trx_volume_min_n
 	and bc.enabled_b = true
-  and bcr.ranking_n <= %(top_n)d;
+  and bcr.ranking_n <= %(top_n)s;
 """
+
+
+UPDATE_BPD_AWARD_WINNER = """
+insert into bpd_citizen.bpd_award_winner(
+	award_period_id_n,
+	fiscal_code_s,
+	payoff_instr_s,
+	amount_n,
+	insert_date_t,
+	insert_user_s,
+	enabled_b,
+	aw_period_start_d,
+	aw_period_end_d,
+	jackpot_n,
+	cashback_n,
+	typology_s,
+	account_holder_cf_s,
+	account_holder_name_s,
+	account_holder_surname_s,
+	check_instr_status_s,
+	technical_account_holder_s,
+	issuer_card_id_s
+)
+""" +  SELECT_BPD_AWARD_WINNER 
+
 
 class Awardwinner() :
   
@@ -67,24 +89,15 @@ class Awardwinner() :
     award_winner_df = pd.read_sql(award_winner_q, self.db_connection)
     print(award_winner_df.to_csv())
 
-  def store_winners(self):
-    award_winner_df = pd.read_csv(self.args.file, sep=';')
-    self._insert_award_winners(award_winner_df)
+  def update_jackpot_winners(self):
+    self.db_cursor = self.db_connection.cursor()
+    update_jackpot_winners_q = self.db_cursor.mogrify(
+      UPDATE_BPD_AWARD_WINNER,
+      {
+        "award_period" : self.args.award_period,
+        "top_n" : 100000,
+      })
 
-  def _insert_award_winners(self, award_winner_df):
-    assert award_winner_df >= 0
-      
-    columns = list(award_winner_df)
-
-    # create VALUES('%s', '%s",...) one '%s' per column
-    values = "VALUES({})".format(",".join(["%s" for _ in columns])) 
-
-    # create INSERT INTO table (columns) VALUES('%s',...)
-    insert = "INSERT INTO {} ({}) {}".format(
-      "bpd_citizen.bpd_award_winner", ",".join(columns), values)
-
-    cursor = self.db_connection.cursor()
-    psycopg2.extras.execute_batch(cursor, insert, award_winner_df.values)
-    if self.args.commit:
-      self.db_connection.commit()
-    cursor.close()
+    self.db_cursor.execute(update_jackpot_winners_q)
+    self.db_connection.commit()
+    self.db_cursor.close()
