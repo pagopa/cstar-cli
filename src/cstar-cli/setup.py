2beafa85c9b2fad3f4ec73a730bdf86b852e932c
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-06-27 15:53:09 +0200

    [RTD-154] add "sender" package

--- src/cstar-cli/setup.py
+++ src/cstar-cli/setup.py
@@ -6,7 +6,7 @@ setup(
    description='Cstar Command Line Interface',
    author='Giovanni Mancini',
    author_email='giovanni.mancini@pagopa.it',
-   packages=['cstar.cli', 'cstar.cli.bpd', 'cstar.cli.rtd', 'cstar.cli.tae'],
+   packages=['cstar.cli', 'cstar.cli.bpd', 'cstar.cli.rtd', 'cstar.cli.tae', 'cstar.cli.sender'],
    install_requires=['cstar-cli-core'], #external packages as dependencies
    scripts=[
       'cst',
