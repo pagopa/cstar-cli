e056f14514c94d1e23877932c21b4a34e9a51337
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-07-07 14:40:47 +0200

    [RTD-242] set "./generated" as default path for expected output generated files

--- src/cstar-cli-core/cstar/cli/core/parser.py
+++ src/cstar-cli-core/cstar/cli/core/parser.py
@@ -123,7 +123,7 @@ def parser():
     sender_aggregates_parser.add_argument("--sender", default=99999, help="Sender code")
     sender_aggregates_parser.add_argument("--avg-trx", type=int, default=10,
                                           help="Average transaction number per aggregation")
-    sender_aggregates_parser.add_argument("-o", "--out-dir", type=str, default=".",
+    sender_aggregates_parser.add_argument("-o", "--out-dir", type=str, default="./generated",
                                           help="Output directory of both files")
     sender_aggregates_parser.add_argument("--shuffle", action="store_true", help="Flag for shuffling transactions file")
 
