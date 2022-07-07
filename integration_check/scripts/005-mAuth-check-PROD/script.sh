c417167ae4f95bbace52c7a139f15a3570178f65
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-07-07 14:38:38 +0200

    [RTD-242] improve log fifth script

--- integration_check/scripts/005-mAuth-check-PROD/script.sh
+++ integration_check/scripts/005-mAuth-check-PROD/script.sh
@@ -2,15 +2,17 @@
 # MAUTH PROD CHECK
 # This script is used to check the validity of PROD mAuth certificate.
 
-CERT=$1
-KEY=$2
-
-if [ $# -ne 2 ]; then
-    echo "Illegal number of parameters" >&2
+if [ $# -ne 2 ] ; then
+    echo "Illegal number of parameters (2 mandatory, was $#)" >&2
+    echo "usage: script.sh /PATH/TO/ACME_PROD.pem /PATH/TO/ACME_PROD.key" >&2
     exit 2
 fi
 
-status_code=$(curl -s -o /dev/null -w "%{http_code}" --cert "$CERT" --key "$KEY" https://api.cstar.pagopa.it/rtd/mauth/check)
+# Parameters:
+# Path to client certificate with .pem extension
+CERT=$1
+# Path to client private key with .key extension
+CERT_KEY=$2
 
 if [ "${status_code}" -eq 200 ]
 then
