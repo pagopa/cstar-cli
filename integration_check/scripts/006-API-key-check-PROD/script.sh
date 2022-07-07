00da92759d1812d0c6f6a65dad25e9bb34752cfb
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-07-07 14:39:02 +0200

    [RTD-242] improve log fifth script

--- integration_check/scripts/006-API-key-check-PROD/script.sh
+++ integration_check/scripts/006-API-key-check-PROD/script.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
-# GENERATED FILE LOCAL EQUALITY CHECK
-# This script is used to check whether the generated file is equal to the expected file.
+# API Key PROD CHECK
+# This script is used to check the validity of PROD mAuth  certificate and API Key.
 
-CERT=$1
-KEY=$2
-API_KEY=$3
-
-if [ $# -ne 3 ]; then
-    echo "Illegal number of parameters" >&2
+if [ $# -ne 3 ] ; then
+    echo "Illegal number of parameters (3 mandatory, was $#)" >&2
+    echo "usage: script.sh /PATH/TO/ACME_PROD.pem /PATH/TO/ACME_PROD.key API_KEY" >&2
     exit 2
 fi
 
-status_code=$(curl -s -o /dev/null -w "%{http_code}" --cert "$CERT" --key "$KEY" --header 'Ocp-Apim-Subscription-Key: '"$API_KEY" https://api.cstar.pagopa.it/rtd/api-key/check)
+# Parameters:
+# Path to client certificate with .pem extension
+CERT=$1
+# Path to client private key with .key extension
+CERT_KEY=$2
+# API Key for RTD Products
+API_KEY=$3
 
 if [ "${status_code}" -eq 200 ]
 then
