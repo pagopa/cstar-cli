8eca490018e8d9d63d2d8ac8d3dde29cb067586e
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-07-07 15:03:10 +0200

    [RTD-243] replace curl in second script

--- integration_check/scripts/002-API-key-check-UAT/script.sh
+++ integration_check/scripts/002-API-key-check-UAT/script.sh
@@ -12,9 +12,9 @@ fi
 # Path to client certificate with .pem extension
 CERT=$1
 # Path to client private key with .key extension
-KEY=$2
+CERT_KEY=$2
 # API Key for RTD Products
 API_KEY=$3
 
 # Make a CURL to UAT endpoint with the certificate, key and API Key
-curl -v --cert "$CERT" --key "$KEY" --header 'Ocp-Apim-Subscription-Key: '"$API_KEY" https://api.uat.cstar.pagopa.it/rtd/api-key/check
+wget --verbose -O - --certificate "$CERT" --private-key "$CERT_KEY" --header 'Ocp-Apim-Subscription-Key: '"$API_KEY" https://api.uat.cstar.pagopa.it/rtd/api-key/check
