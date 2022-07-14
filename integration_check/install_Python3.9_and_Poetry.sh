dc45d747a100471e9a4a022da4f14f742943e696
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-07-14 12:39:17 +0200

    [RTD-276] remove download and unzip step of cli (should be already present)

--- integration_check/install_Python3.9_and_Poetry.sh
+++ integration_check/install_Python3.9_and_Poetry.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-apt-get install -y unzip software-properties-common python3-distutils
+apt-get install -y software-properties-common python3-distutils
 apt-get update
 add-apt-repository -y ppa:deadsnakes/ppa
 echo "8 41" | apt install -yq python3.9
 apt-get install -y python3.9-distutils
-wget -O ./cstar-cli.zip https://github.com/pagopa/cstar-cli/archive/refs/heads/main.zip
-unzip cstar-cli.zip
 wget https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py
 echo "y" | python3.9 get-poetry.py
