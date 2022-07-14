8d30f73e52d6bc798d80fcade48e88a9f50e898e
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-07-14 10:37:14 +0200

    [RTD-274] update install script

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
+#!/bin/sh
 # Make sure to run this command inside a Poetry shell
 
+python -m pip install --upgrade pip
+pip install numpy
+pip install pandas
 pip install --force-reinstall src/cstar-cli-core
 pip install --no-deps --force-reinstall src/cstar-cli
