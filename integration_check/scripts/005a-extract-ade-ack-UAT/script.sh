4cc0ea8dec2fc90b97ec027e6d4edfe3008f8e60
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-08-03 15:39:15 +0200

    [RTD-324] add mechanism to expect various ade acks status

--- integration_check/scripts/005a-extract-ade-ack-UAT/script.sh
+++ integration_check/scripts/005a-extract-ade-ack-UAT/script.sh
@@ -3,11 +3,11 @@
 # This script is used to extract a fake ADE ACK file from the file deposited from the batch service to SFTP.
 # Once extracted the file is uploaded to the SFTP server where the batch service will download it.
 
-if [ $# -ne 4 ] ; then
-    echo "Illegal number of parameters (4 mandatory, was $#)" >&2
-    echo "usage: script.sh /PATH/TO/DOWNLOADED.csv /PATH/TO/COMPANY_NAME_UAT.pem /PATH/TO/COMPANY_NAME_UAT.key API_KEY" >&2
-    exit 2
-fi
+#if [ $# -ne 4 ] ; then
+#    echo "Illegal number of parameters (4 mandatory, was $#)" >&2
+#    echo "usage: script.sh /PATH/TO/DOWNLOADED.csv /PATH/TO/COMPANY_NAME_UAT.pem /PATH/TO/COMPANY_NAME_UAT.key API_KEY" >&2
+#    exit 2
+#fi
 
 ## Parameters:
 ## Path to local output file downloaded during the previous script.
@@ -20,7 +20,7 @@ CERT_KEY=$3
 API_KEY=$4
 
 # Number of lines extracted at random that will form the fake ADE ACK file.
-NUM_RANDOM_ACKED_LINES=3
+NUM_RANDOM_ACKED_LINES=5
 
 URL="https://api.uat.cstar.pagopa.it/rtd/sftp-deposit/"
 
@@ -44,10 +44,28 @@ shuf -n "$NUM_RANDOM_ACKED_LINES" "$LOCAL" > "./$TEMPORARY_DIR/extracted_lines.c
 # Remove zero-width-spacing and CR at the end of the downloaded file
 tr -d '\015' < "./$TEMPORARY_DIR/extracted_lines.csv" | sed 's/\xef\xbb\xbf//g' > "./$TEMPORARY_DIR/extracted_lines_cleaned.csv"
 
+# Counters for ade acks errors and success
+i=0
+
 # Generate a fake ADE ACK record for each line
 while read -r p; do
-  echo "$p" | awk -F ";" '{print($1";4;1201|1302")}' >> "./$TEMPORARY_DIR/$ADE_ACK_NAME"
-  echo "$p" | awk -F ";" '{print($10";"$11";"$12";"$9";1201|1302")}' >> "./$TEMPORARY_DIR/$ADE_ACK_NAME.expected"
+  if [ $i -eq 0 ]
+    then
+      echo "$p" | awk -F ";" '{print($1";0;")}' >> "./$TEMPORARY_DIR/$ADE_ACK_NAME"  else
+  fi
+  if [ $i -eq 1 ]
+    then
+      echo "$p" | awk -F ";" '{print($1";1;")}' >> "./$TEMPORARY_DIR/$ADE_ACK_NAME"  else
+      echo "$p" | awk -F ";" '{print($10";"$11";"$12";"$9";1;")}' >> "./$TEMPORARY_DIR/$ADE_ACK_NAME.expected"
+  fi
+  if [ $i -eq 2 ]
+    then
+      echo "$p" | awk -F ";" '{print($1";3;")}' >> "./$TEMPORARY_DIR/$ADE_ACK_NAME"  else
+      echo "$p" | awk -F ";" '{print($10";"$11";"$12";"$9";3;")}' >> "./$TEMPORARY_DIR/$ADE_ACK_NAME.expected"
+    else
+      echo "$p" | awk -F ";" '{print($1";4;1201|1302")}' >> "./$TEMPORARY_DIR/$ADE_ACK_NAME"
+      echo "$p" | awk -F ";" '{print($10";"$11";"$12";"$9";4;1201|1302")}' >> "./$TEMPORARY_DIR/$ADE_ACK_NAME.expected"
+  fi
 done < "./$TEMPORARY_DIR/extracted_lines_cleaned.csv"
 
 # Check for content of temporary file
@@ -69,14 +87,14 @@ cp "./$TEMPORARY_DIR/$ADE_ACK_NAME.expected" "./generated/ade-acks/$ADE_ACK_NAME
 gzip "./$TEMPORARY_DIR/$ADE_ACK_NAME"
 
 # Upload the file through the APIM
-wget --verbose \
-    -O - \
-    --method=PUT \
-    --body-file "./$TEMPORARY_DIR/$ADE_ACK_NAME.gz" \
-    --certificate "$CERT" \
-    --private-key "$CERT_KEY" \
-    --header 'Ocp-Apim-Subscription-Key: '"$API_KEY" \
-    "$URL$ADE_ACK_NAME.gz"
+#wget --verbose \
+#    -O - \
+#    --method=PUT \
+#    --body-file "./$TEMPORARY_DIR/$ADE_ACK_NAME.gz" \
+#    --certificate "$CERT" \
+#    --private-key "$CERT_KEY" \
+#    --header 'Ocp-Apim-Subscription-Key: '"$API_KEY" \
+#    "$URL$ADE_ACK_NAME.gz"
 
 # Check for content of temporary file
 if [ -z "$(cat "./generated/ade-acks/$ADE_ACK_NAME.expected")" ]
