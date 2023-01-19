d846233bf78c8cb39c095374517ab81536c59b04
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-01-19 14:21:44 +0100

    [RTD-1228] add utilities for file and string pgp encryption

--- src/cstar-cli/cstar/cli/idpay/idpay_utilities.py
+++ src/cstar-cli/cstar/cli/idpay/idpay_utilities.py
@@ -1,5 +1,9 @@
+import io
+import logging
 import os
+import tempfile
 
+import gnupg
 import pandas as pd
 from dateutil import parser
 
@@ -35,3 +39,43 @@ def flatten_values(dataset):
         for j in dataset[i]:
             res.append(j)
     return res
+
+def pgp_string(payload: str, pgp_key_data: str):
+    with tempfile.TemporaryDirectory() as temp_gpg_home:
+        gpg = gnupg.GPG(gnupghome=temp_gpg_home)
+        import_result = gpg.import_keys(pgp_key_data)
+        with io.StringIO(payload) as to_be_encrypted:
+            with tempfile.NamedTemporaryFile(delete=False, suffix='.pgp', mode='r+b') as f:
+                status = gpg.encrypt_file(
+                    to_be_encrypted,
+                    recipients=import_result.results[0]["fingerprint"],
+                    output=f.name,
+                    extra_args=["--openpgp", "--trust-model", "always"],
+                    armor=True,
+                )
+                if status.ok:
+                    f.seek(0)
+                    return f.read()
+                else:
+                    logging.error("Failed to pgp")
+    return None
+
+def pgp_file(file_path: str, pgp_key_data: str):
+    with tempfile.TemporaryDirectory() as temp_gpg_home:
+        gpg = gnupg.GPG(gnupghome=temp_gpg_home)
+        import_result = gpg.import_keys(pgp_key_data)
+        logging.info(f"GPG import keys: {import_result.results}")
+        output_path = f"{file_path}.pgp"
+        with open(file_path, "rb") as f:
+            status = gpg.encrypt_file(
+                f,
+                recipients=import_result.results[0]["fingerprint"],
+                output=output_path,
+                extra_args=["--openpgp", "--trust-model", "always"],
+                armor=False,
+            )
+            if status.ok:
+                return output_path
+            else:
+                logging.error("Failed to pgp")
+    return None
