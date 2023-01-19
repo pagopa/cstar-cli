8be9cd2f480938da178d669e497ae66f10f95261
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-01-19 17:33:57 +0100

    [RTD-1228] add reward command

---
+++ src/cstar-cli/cstar/cli/idpay/idpayrewards.py
@@ -0,0 +1,63 @@
+import random
+from datetime import datetime
+from hashlib import sha256
+
+import pandas as pd
+from .idpay_utilities import serialize
+
+reward_columnds = [
+    'uniqueID',
+    'result',
+    'rejectionReason',
+    'cro',
+    'executionDate'
+]
+possible_results = [
+    'OK - ORDINE ESEGUITO',
+    'KO'
+]
+possible_rejectReasons = [
+    'IBAN NOT VALID'
+]
+
+
+class IDPayRewards:
+    def __init__(self, args):
+        self.args = args
+
+    def rewards(self):
+        with open(self.args.payment_provisions_export, 'r') as input_file:
+            input_file.seek(0)
+            payments = pd.read_csv(input_file, quotechar='"', usecols=['uniqueID'], sep=';')
+
+
+            curr_execution_date = self.args.exec_date
+            if self.args.exec_date is None:
+                curr_execution_date = datetime.now().strftime('%Y-%m-%d')
+
+            rewards = []
+            i = 0
+            for payment in payments.values:
+
+                # Decide whether the reward is OK or KO based on desired probability
+                if random.random() < self.args.perc_succ:
+                    curr_result = possible_results[0]
+                    curr_reject_reason = ''
+                else:
+                    curr_result = possible_results[1]
+                    curr_reject_reason = possible_rejectReasons[0]
+
+                rewards.append([
+                    payment[0],
+                    curr_result,
+                    curr_reject_reason,
+                    sha256(f"{i}".encode()).hexdigest(),
+                    curr_execution_date
+                ])
+
+            # Insert desired duplicates
+            duplicates = random.sample(rewards, round(self.args.perc_dupl*len(rewards)))
+            for d in duplicates:
+                rewards.append(d)
+
+            serialize(rewards, reward_columnds, './rewards-dispositive-'+datetime.now().isoformat()+'.csv')
