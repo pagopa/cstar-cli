46b3c1c98e83dd8a85421046738e46325183e7b5
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-01-31 11:14:31 +0100

    [RTD-1228] change file encryption library

--- src/cstar-cli/cstar/cli/idpay/idpay_utilities.py
+++ src/cstar-cli/cstar/cli/idpay/idpay_utilities.py
@@ -1,9 +1,6 @@
-import logging
 import os
-import tempfile
 
 import pgpy
-import gnupg
 import pandas as pd
 from dateutil import parser
 
@@ -47,25 +44,17 @@ def flatten_values(dataset):
 def pgp_string(payload: str, pgp_key_data: str):
     key = pgpy.PGPKey.from_blob(pgp_key_data)
     message = pgpy.PGPMessage.new(payload)
-    ciphertext = key[0].encrypt(message,version=b'BCPG v1.58')
+    ciphertext = key[0].encrypt(message, version=b'BCPG v1.58')
     return str(ciphertext)
 
+
 def pgp_file(file_path: str, pgp_key_data: str):
-    with tempfile.TemporaryDirectory() as temp_gpg_home:
-        gpg = gnupg.GPG(gnupghome=temp_gpg_home)
-        import_result = gpg.import_keys(pgp_key_data)
-        logging.info(f"GPG import keys: {import_result.results}")
-        output_path = f"{file_path}.pgp"
-        with open(file_path, "rb") as f:
-            status = gpg.encrypt_file(
-                f,
-                recipients=import_result.results[0]["fingerprint"],
-                output=output_path,
-                extra_args=["--openpgp", "--trust-model", "always"],
-                armor=False,
-            )
-            if status.ok:
-                return output_path
-            else:
-                logging.error("Failed to pgp")
-    return None
+    key = pgpy.PGPKey.from_blob(pgp_key_data)
+    with open(file_path, "rb") as f:
+        message = pgpy.PGPMessage.new(f.read(), file=True)
+    encrypted = key[0].encrypt(message, openpgp=True)
+    output_path = f"{file_path}.pgp"
+    with open(output_path, "wb") as f:
+        f.write(bytes(encrypted))
+
+    return output_path
